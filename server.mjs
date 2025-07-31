fb8f6daaa71833b993a4a8e8e8697bb9ca497fa6
Martin Heidegger <martin.heidegger@gmail.com>
2025-08-01 04:49:57 +0900

    amend

--- server.mjs
+++ server.mjs
@@ -1,4 +1,4 @@
-import fs from 'node:fs/promises'
+import { readFile, writeFile, readdir, unlink } from 'node:fs/promises'
 import express from 'express'
 import 'temporal-polyfill/global'
 import '@formatjs/intl-durationformat/polyfill.js'
@@ -7,11 +7,7 @@ import '@formatjs/intl-durationformat/polyfill.js'
 const isProduction = process.env.NODE_ENV === 'production'
 const port = process.env.PORT || 5173
 const base = process.env.BASE || '/'
-
-// Cached production assets
-const templateHtml = isProduction
-  ? await fs.readFile('./dist/client/index.html', 'utf-8')
-  : ''
+const isBuild = process.env.BUILD === "1"
 
 // Create http server
 const app = express()
@@ -19,20 +15,16 @@ const app = express()
 // Add Vite or respective production middlewares
 /** @type {import('vite').ViteDevServer | undefined} */
 let vite
-if (!isProduction) {
-  const { createServer } = await import('vite')
-  vite = await createServer({
-    server: { middlewareMode: true },
-    appType: 'custom',
-    base,
-  })
-  app.use(vite.middlewares)
-} else {
-  const compression = (await import('compression')).default
-  const sirv = (await import('sirv')).default
-  app.use(compression())
-  app.use(base, sirv('./dist/client', { extensions: [] }))
-}
+const { createServer } = await import('vite')
+vite = await createServer({
+  server: {
+    middlewareMode: true,
+    hmr: !isBuild
+  },
+  appType: 'custom',
+  base,
+})
+app.use(vite.middlewares)
 
 // Serve HTML
 app.use('*', async (req, res) => {
@@ -43,22 +35,25 @@ app.use('*', async (req, res) => {
     let template
     /** @type {import('./src/entry-server.js').render} */
     let render
-    if (!isProduction) {
-      // Always read fresh template in development
-      template = await fs.readFile('./src/example/index.html', 'utf-8')
-      template = await vite.transformIndexHtml(url, template)
-      render = (await vite.ssrLoadModule('/src/example/entry-server.jsx'))
-        .render
-    } else {
-      template = templateHtml
-      render = (await import('./dist/server/entry-server.js')).render
-    }
+    // Always read fresh template in development
+    template = await readFile('./index.html', 'utf-8')
+    template = await vite.transformIndexHtml(url, template)
+    render = (await vite.ssrLoadModule('/src/example/entry-server.jsx'))
+      .render
 
     const rendered = await render(url)
 
-    const html = template
+    let html = template
       .replace(`<!--app-head-->`, rendered.head ?? '')
       .replace(`<!--app-html-->`, rendered.html ?? '')
+
+    const dir = 'client/assets'
+    const target = `./${dir}/${(await readdir(`./dist/${dir}`))[0]}`
+    if (isBuild) {
+      html = html
+        .replace('<script type="module" src="/@vite/client"></script>', '')
+        .replace('/src/example/entry-client.jsx', target)
+    }
 
     res.status(200).set({ 'Content-Type': 'text/html' }).send(html)
   } catch (e) {
@@ -69,6 +64,34 @@ app.use('*', async (req, res) => {
 })
 
 // Start http server
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`)
+  if (!isBuild) {
+    return
+  }
+
+  ;(async () => {
+    console.log('Loading resources...')
+    // Fetching the site finishes when all is built
+    const res = await fetch(`http://localhost:${port}`)
+    const body = await res.arrayBuffer()
+    await writeFile("./dist/index.html", new Uint8Array(body))
+    await unlink("./dist/client/index.html").catch(() => {})
+    console.log('...finished')
+    console.log('Closing server')
+    server.close(() => {
+      console.log('done')
+      process.exit()
+    })
+  })()
+    .then(
+      () => {},
+      error => {
+        console.error(error)
+        server.close((error) => {
+          console.error(error)
+          process.exit(1)
+        })
+      }
+    )
 })
